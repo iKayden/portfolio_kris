eb75c254229e050a1c018d4b01b8e26ad8f8e9e4
ikayden <ikyden23@gmail.com>
2023-10-03 14:37:55 -0700

    add burger menu. fix css bug

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,25 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./navbar.css";
 import logo from "../../assets/logo_grey.png";
 import { Link } from 'react-scroll';
 import contactImg from "../../assets/contact.png";
+import menu from "../../assets/menu.png";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
   return (
     <nav className='navbar'>
       <img src={logo} alt='logo' className='navbar-logo'></img>
       <div className='navbar-menu'>
-        <Link className='navbar-menu__item'>Home</Link>
-        <Link className='navbar-menu__item'>About</Link>
-        <Link className='navbar-menu__item'>Showreel</Link>
-        <Link className='navbar-menu__item'>Portfolio</Link>
+        <Link activeClass='active'
+          to='main' spy={true} smooth={true} offset={-200} duration={500} className='navbar-menu__item'>
+          Home
+        </Link>
+        <Link activeClass='active' to='about' spy={true} smooth={true}
+          offset={-100} duration={500} className='navbar-menu__item'>
+          About
+        </Link>
+        {/* <Link activeClass='active' to='portfolio' spy={true}
+          smooth={true} offset={-100} duration={500}
+          className='navbar-menu__item'>Showreel</Link> */}
+        <Link activeClass='active' to='portfolio' spy={true}
+          smooth={true} offset={-100} duration={500}
+          className='navbar-menu__item'>
+          Portfolio
+        </Link>
       </div>
 
-      <button className='navbar-contactButton'>
+      <button className='navbar-contactButton' onClick={() => {
+        document.getElementById('contact-container').scrollIntoView({ behavior: "smooth" });
+      }}>
         <img src={contactImg} alt='' className='navbar-contactButton__image' />
         Contact Me
       </button>
-    </nav>
+
+      <img src={menu} alt='menu' className='navbar-burger' onClick={() => setShowMenu(!showMenu)} />
+      <div className='navbar-menu__burger' style={{ display: showMenu ? "flex" : "none" }} >
+        <Link activeClass='active'
+          to='main' spy={true} smooth={true} offset={-200} duration={500} className='navbar-menu__burger-item' onClick={() => setShowMenu(false)}>
+          Home
+        </Link>
+        <Link activeClass='active' to='about' spy={true} smooth={true}
+          offset={-50} duration={500} className='navbar-menu__burger-item' onClick={() => setShowMenu(false)}>
+          About
+        </Link>
+        <Link activeClass='active' to='contact-container' spy={true}
+          smooth={true} offset={-100} duration={500}
+          className='navbar-menu__burger-item' onClick={() => setShowMenu(false)}>
+          Portfolio
+        </Link>
+        <Link activeClass='active' to='portfolio' spy={true}
+          smooth={true} offset={-100} duration={500}
+          className='navbar-menu__burger-item' onClick={() => setShowMenu(false)}>Contact</Link>
+      </div>
+
+    </nav >
   );
 };
 
